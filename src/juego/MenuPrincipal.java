d1f6b42d51e66b4a28265ec7add2b289534c7baa
DESKTOP-8DOAGSQ <edvelasquezv@unal.edu.co>
2018-02-28 23:05:33 -0500

    EDGAR:  creacion primera class para interfaz grafica

---
+++ src/juego/MenuPrincipal.java
@@ -0,0 +1,16 @@
+package juego;
+
+import java.awt.Frame;
+import java.awt.TextArea;
+import java.awt.TextField;
+import javax.swing.JButton;
+
+public class MenuPrincipal {
+	
+	
+	public MenuPrincipal() {
+		
+		
+	}
+
+}
